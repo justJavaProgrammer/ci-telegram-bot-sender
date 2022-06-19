4b520a80ae5cf39881d24ddaf1d8487962e669c2
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-19 18:15:03 +0300

    Added exit code

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const token = '5449681342:AAGXAxO90zoyiG7i-PefcF67Mkrm49L8NMM';
 const bot = new TelegramBot(token, {polling: true});
 const repoName = core.getInput('repository-name');
 bot.sendMessage('1218397122', `Repository: ${repoName}`);
-
+process.exit(0);
