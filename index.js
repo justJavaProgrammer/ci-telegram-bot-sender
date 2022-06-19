847d17ed529fca7dcf18ecbba5cc0b8e1c0cee4e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-19 20:55:07 +0300

    Version 1.6.1

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const github = require('@actions/github');
 const token = '5449681342:AAGXAxO90zoyiG7i-PefcF67Mkrm49L8NMM';
 
 const bot = new TelegramBot(token, {polling: true});
-const repoName = core.getInput('repository-name');
-bot.sendMessage('1218397122', `Repository: ${repoName}`).then(() => {
+const sender =  github.context.payload.sender;
+const action = github.context.payload.action;
+const repo = github.context.payload.repository;
+bot.sendMessage('1218397122', `Hello, ${sender}, you are triggered event: ${action} in repository: ${repo.name}, url" ${repo.html_url}`).then(() => {
     console.log("Done")
     process.exit(0);
 });
