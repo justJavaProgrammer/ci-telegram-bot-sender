15a08f322778ded7ed526c819c5264e790e15dc7
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-19 17:49:28 +0300

    Updated

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 const token = '5449681342:AAGXAxO90zoyiG7i-PefcF67Mkrm49L8NMM';
 
 const bot = new TelegramBot(token, {polling: true});
+const repoName = core.getInput('repository-name');
+bot.sendMessage('1218397122', `Repository: ${repoName}`);
 
-bot.on('message', function (msg) {
-    console.log(msg);
-    bot.sendMessage(msg.chat.id, `You tapped: ${msg.text}`);
-})
